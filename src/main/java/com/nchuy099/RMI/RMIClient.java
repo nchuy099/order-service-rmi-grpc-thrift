d0fed846164d37ec9afa33848b33a1529c6fc685
nchuy099 <nchihuy099@gmail.com>
2025-02-23 12:16:06 +0700

    change server's hostname

--- src/main/java/com/nchuy099/RMI/RMIClient.java
+++ src/main/java/com/nchuy099/RMI/RMIClient.java
@@ -7,8 +7,8 @@ public class RMIClient {
     public static void main(String[] args) {
         try {
             String productId = "1";
-            int quantity = 10;
-            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            int quantity = 3;
+            Registry registry = LocateRegistry.getRegistry("192.168.67.50", 1099);
             Order stub = (Order) registry.lookup("OrderService");
             System.out.println("Total cost for " + quantity + " with id " + productId + ": " + stub.calculateTotal(productId, quantity));
         } catch (Exception e) {
